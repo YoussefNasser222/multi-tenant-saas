ff7aeaecae725cf701a638e77f1239cfdc3d9402
youssef Nasser <kakahoba012@gmail.com>
2026-08-25 22:25:38 +0300

    update node mailer

--- src/common/helpers/sendMail.helper.ts
+++ src/common/helpers/sendMail.helper.ts
@@ -1,13 +1,17 @@
-import nodemailer from "nodemailer"
-import { MailOptions } from "nodemailer/lib/sendmail-transport"
+import { log } from 'console';
+import nodemailer from 'nodemailer';
+import { MailOptions } from 'nodemailer/lib/sendmail-transport';
 export const sendMail = async (mailOptions: MailOptions) => {
-    const transporter = nodemailer.createTransport({
-        service: "gmail",
-        port: parseInt(process.env.EMAIL_PORT || "587"),
-        auth: {
-            user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASSWORD
-        }
-    })
-    await transporter.sendMail(mailOptions)
-}
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    port: parseInt(process.env.EMAIL_PORT || '587'),
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  });
+  await transporter.sendMail({
+    from: `"ARC-Community" <${process.env.EMAIL_USER}>`,
+    ...mailOptions,
+  });
+};
